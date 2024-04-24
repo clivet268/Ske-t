d3f668b21b931583d0071e469a7db53349c8231b
EvanRosenb01 <evanrosenblatt01@gmail.com>
2024-04-24 13:30:05 -0400

    Modified mostly the GamePlayer. Also changed aroiu nd the variables for entities and skateboard.

--- Ske-t.srcs/sources_1/new/PlayRenderer.v
+++ Ske-t.srcs/sources_1/new/PlayRenderer.v
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
 
 // x = Seven seg an selector
-// entity = [0000 = skateboard manhole bird pole] 
+// entity = [000 = manhole bird pole] 
 // TODO how should entities be rendered? if entities have set seegments then just an array of entity types should do
-module PlayRenderer(input clock, input[3:0] x, input[3:0] y, output [7:0] seg);
-    reg[7:0] ourbuf= 0;
+module PlayRenderer(input clock, input[3:0] x, input[2:0] entities, input[2:0] skateboard, output [7:0] seg);
+    reg[7:0] ourbuf= 8'b00000000;
     always @(posedge clock) 
         begin
-            ourbuf = ourbuf+1;
+            ourbuf = ourbuf+8'b00000001;
         //((~x & 4'b0101) != 4'b0000);
         end
         assign seg = ourbuf;
