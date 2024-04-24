126da013d41cde416d9d397c824169fa639da5ea
clivet268 <clivet268@gmail.com>
2024-04-24 13:30:03 -0400

    TB WORKED, ground rendering

--- Ske-t.srcs/sim_1/new/GroundTB.v
+++ Ske-t.srcs/sim_1/new/GroundTB.v
@@ -1,32 +1,26 @@
 `timescale 1ns / 1ps
 
 module GroundTB();
-    reg clock;
+    reg clock = 0;
     reg[3:0] x;
     reg[3:0] y;
-    wire[7:0] seg = 8'b00000000;
+    wire[7:0] seg;
+    always #5 clock = ~clock;
     PlayRenderer u0(clock, x, y, seg);
         initial begin
-        clock = 0;
         x = 4'b1110;
         y = 4'b0000;
         #50;
-        clock = 1;
         x = 4'b1110;
         #50;
-        clock = 0;
         x = 4'b1101;
         #50;
-        clock = 1;
         x = 4'b1011;
         #50;
-        clock = 0;
         x = 4'b0111;
         #50;
-        clock = 1;
         x = 4'b1110;
         #50;
-        clock = 0;
         x = 4'b1101;
         #50;
         end
--- Ske-t.srcs/sources_1/new/PlayRenderer.v
+++ Ske-t.srcs/sources_1/new/PlayRenderer.v
@@ -4,11 +4,15 @@
 // entity = [0000 = skateboard manhole bird pole] 
 // TODO how should entities be rendered? if entities have set seegments then just an array of entity types should do
 module PlayRenderer(input clock, input[3:0] x, input[3:0] y, output [7:0] seg);
-    reg[7:0] ourbuf= 0;
+    reg temp = 0;
     always @(posedge clock) 
         begin
-            ourbuf = ourbuf+1;
-        //((~x & 4'b0101) != 4'b0000);
+            temp = ((~x & 4'b1010) != 0) ? 1 : 0;
         end
-        assign seg = ourbuf;
+        assign seg[0] = temp;
+    always @(negedge clock) 
+        begin
+            temp = ((~x & 4'b1010) != 0) ? 0 : 1;
+        end
+        assign seg[0] = temp;
 endmodule
