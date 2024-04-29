e5797f215e06f55f82f6a71b8c8dce41dc54b006
clivet268 <clivet268@gmail.com>
2024-04-29 18:18:54 -0400

    Time to debug

--- Ske-t.srcs/sources_1/new/GamePlayer.v
+++ Ske-t.srcs/sources_1/new/GamePlayer.v
@@ -1,21 +1,22 @@
 `timescale 1ns / 1ps
 
 module GamePlayer(input gameclock, input[3:0] an, input[4:0] btns, output [7:0] seg, output reg [9:0] score, output reg gamestate);
-    // 000 - nothing 001 - manhole 010 - bird 100 - pole
+    // 000 - nothing 001 - manhole 010 - wrench 100 - bird
     reg [2:0] entities_pos [3:0];
     reg [1:0] skateboard_pos = 2'b00;
     reg [2:0] skateboard = 3'b000;
     reg [5:0] period_count = 0; 
     reg [2:0] next_entity = 3'b000;
+    reg [2:0] current_render ;
     integer i = 0;
     reg tick = 0;
     
-    PlayRenderer u2(gameclock, an, entities_pos, skateboard, seg);
+    PlayRenderer u2(gameclock, an, entities_pos[current_render], skateboard, seg);
 /*
     T18 Up Jump btns(0) 
     U17 Down Duck btns(1)
-    W19 Left Advance btns(2)
-    U18 Center Dodge btns(3)
+    U18 Center Dodge btns(2)
+    W19 Left Advance btns(3)
     T17 Right Receed btns(4)
 */
 
@@ -23,8 +24,14 @@ module GamePlayer(input gameclock, input[3:0] an, input[4:0] btns, output [7:0] 
     // is this better or should it sync with ticks
     always @(posedge gameclock)
         begin
-            
-            if (period_count!= 60)
+            case(an)
+            4'b1110: current_render = 0;
+            4'b1101: current_render = 1;
+            4'b1011: current_render = 2;
+            4'b0111: current_render = 3;
+            default: current_render = 0;
+            endcase
+            if (period_count!= 100)
                 begin
                     period_count<= period_count + 1;
                     tick <= 0; 
@@ -43,13 +50,13 @@ module GamePlayer(input gameclock, input[3:0] an, input[4:0] btns, output [7:0] 
         begin
             case(btns)
                 1: skateboard=3'b001; //Jump
-                2: skateboard=3'b010; //duck
+                2: skateboard=3'b010; //Duck
                 4: skateboard=3'b011; //Advance 
                 8: skateboard=3'b101; //Dodge
                 16: skateboard=3'b100; //Receed
-            default: skateboard=3'b000; //stand
+            default: skateboard=3'b000; //Stand
             endcase
-            if (entities_pos[skateboard_pos] != btns) begin
+            if (entities_pos[skateboard_pos] != btns[2:0]) begin
                 gamestate = 0;
             end else begin
                 score <= score + 1;
--- Ske-t.srcs/sources_1/new/PlayRenderer.v
+++ Ske-t.srcs/sources_1/new/PlayRenderer.v
@@ -1,17 +1,30 @@
 `timescale 1ns / 1ps
 
 // x = Seven seg an selector
-// entity = [xx = manhole bird pole] 
-module PlayRenderer(input clock, input[3:0] an, reg [2:0] entities [3:0], input[2:0] skateboard, output [7:0] seg);
+// entity = [xxx = manhole wrench bird] 
+module PlayRenderer(input clock, input[3:0] an, input [2:0] entities, input[2:0] skateboard, output reg [7:0] seg);
+    
     reg temp = 0;
     always @(posedge clock) 
         begin
-            temp = ((~x & 4'b1010) != 0) ? 1 : 0;
+            case(skateboard)
+                3'b000: seg <= seg & 8'b11110011;//Stand
+                3'b001: seg <= seg & 8'b11111011;//Jump
+                3'b010: seg <= seg & 8'b11110111;//Duck
+                3'b101: seg <= seg & 8'b10011111;//Dodge
+                default: seg <= seg & 8'b11110011;//Stand
+            endcase
+            case(skateboard)
+                3'b001: seg <= seg & 8'b11101111;//manhole
+                3'b010: seg <= seg & 8'b01111111;//wrench
+                3'b100: seg <= seg & 8'b11111101;//bird
+            endcase
+            temp = ((~an & 4'b1010) != 0) ? 1 : 0;
+            seg[0] <= temp;
         end
-        assign seg[0] = temp;
     always @(negedge clock) 
         begin
-            temp = ((~x & 4'b1010) != 0) ? 0 : 1;
+            temp = ((~an & 4'b1010) != 0) ? 0 : 1;
+            seg[0] <= temp; 
         end
-        assign seg[0] = temp;
 endmodule
